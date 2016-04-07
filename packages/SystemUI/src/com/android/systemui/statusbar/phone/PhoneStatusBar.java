acd875d857b6c043e972dc2cd67fd8d61a86c1b9
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-04-08 01:05:19 +0200

    Fw_base - Ambient display: Battery status (1/2)
    - Show/hide battery status on ambient display
    
    rogersb11 Fix this with all our stuff
    Change KeyguardStatusView setup from view to allow control as doze isn't
    attached to "View"
    
    Change-Id: I67d9419623618f70b405b03bba74414ad90226c8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -136,6 +136,7 @@ import com.android.internal.utils.du.ActionHandler;
 import com.android.internal.utils.du.DUPackageMonitor;
 import com.android.internal.utils.du.DUSystemReceiver;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
+import com.android.keyguard.KeyguardStatusView;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
@@ -405,7 +406,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // top bar
     BaseStatusBarHeader mHeader;
     protected KeyguardStatusBarView mKeyguardStatusBar;
-    View mKeyguardStatusView;
+    KeyguardStatusView mKeyguardStatusView;
     KeyguardBottomAreaView mKeyguardBottomArea;
     boolean mLeaveOpenOnKeyguardHide;
     KeyguardIndicationController mKeyguardIndicationController;
@@ -1077,7 +1078,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mVisualizerView = (VisualizerView) mStatusBarWindow.findViewById(R.id.visualizerview);
 
         mKeyguardStatusBar = (KeyguardStatusBarView) mStatusBarWindow.findViewById(R.id.keyguard_header);
-        mKeyguardStatusView = mStatusBarWindow.findViewById(R.id.keyguard_status_view);
+        mKeyguardStatusView = (KeyguardStatusView) mStatusBarWindow.findViewById(R.id.keyguard_status_view);
         mKeyguardBottomArea =
                 (KeyguardBottomAreaView) mStatusBarWindow.findViewById(R.id.keyguard_bottom_area);
         mKeyguardBottomArea.setActivityStarter(this);
@@ -4791,6 +4792,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mDozeScrimController.setDozing(mDozing &&
                 mFingerprintUnlockController.getMode()
                         != FingerprintUnlockController.MODE_WAKE_AND_UNLOCK_PULSING, animate);
+        mKeyguardStatusView.setDozing(mDozing);
         mVisualizerView.setDozing(mDozing);
         Trace.endSection();
     }
