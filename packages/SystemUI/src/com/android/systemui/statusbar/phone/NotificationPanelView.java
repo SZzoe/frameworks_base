b7afea628ad65b223204f88603445129a7ef229e
LorDClockaN <lordclockan@gmail.com>
2016-08-14 22:14:37 +0200

    Fw_base - Blur: Fix qs shade alpha to show correctly after LS
    Also get qs stroke to show up while blur is enabled
    
    Change-Id: I6e4666e3ac2ad1b2e1e547db5477dfdf0462db43
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -390,10 +390,10 @@ public class NotificationPanelView extends PanelView implements
 
         mBlurredView.setVisibility(View.INVISIBLE);
 
+        setQSStroke();
         if (mTranslucentQuickSettings) {
             handleQuickSettingsBackround();
         } else {
-            setQSStroke();
             setQSBackgroundAlpha();
         }
     }
@@ -418,8 +418,12 @@ public class NotificationPanelView extends PanelView implements
         }
         if (mNotificationPanelView == null)
             return;
-        if (mKeyguardShowing || mHeadsUpShowing || mHeadsUpAnimatingAway)
+        if (mHeadsUpShowing || mHeadsUpAnimatingAway)
             return;
+        if (mTranslucentQuickSettings) {
+            if (mKeyguardShowing)
+                return;
+        }
 
         BlurTask.setBlurTaskCallback(new BlurUtils.BlurTaskCallback() {
 
@@ -494,7 +498,6 @@ public class NotificationPanelView extends PanelView implements
         mBlurredView.setTag("ready_to_blur");
 
         mBlurredView.setVisibility(View.INVISIBLE);
-
     }
 
     public static class BlurTask extends AsyncTask<Void, Void, Bitmap> {
@@ -1388,7 +1391,11 @@ public class NotificationPanelView extends PanelView implements
         updateQsState();
 
         try {
-            handleQuickSettingsBackround();
+            if (mTranslucentQuickSettings) {
+                handleQuickSettingsBackround();
+            } else {
+                setQSBackgroundAlpha();
+            }
         } catch (Exception e){
         }
     }
@@ -2831,6 +2838,7 @@ public class NotificationPanelView extends PanelView implements
             mTranslucencyPercentage = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, 60);
 
+            setQSStroke();
             if (mTranslucentQuickSettings) {
                 mBlurDarkColorFilter = Color.LTGRAY;
                 mBlurMixedColorFilter = Color.GRAY;
@@ -2838,20 +2846,14 @@ public class NotificationPanelView extends PanelView implements
                 mTranslucencyPercentage = 255 - ((mTranslucencyPercentage * 255) / 100);
                 handleQuickSettingsBackround();
             } else {
-                setQSStroke();
                 setQSBackgroundAlpha();
             }
         }
     }
 
     private void setQSBackgroundAlpha() {
-        if (Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.QS_TRANSPARENT_SHADE, 255) != 255) {
-            if (mQsContainer != null) {
-                mQsContainer.getBackground().setAlpha(mQSShadeAlpha);
-            }
-        } else {
-            mQsContainer.getBackground().setAlpha(255);
+        if (mQsContainer != null) {
+            mQsContainer.getBackground().setAlpha(mQSShadeAlpha);
         }
     }
 
