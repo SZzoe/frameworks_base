b2728a2197f15c024b064e613ce416777612a16a
Varun Date <date.varun123@gmail.com>
2016-12-15 22:39:06 +0300

    Fw_base - Add a Switch to Enable/Disable Dynamic Navbar [1/2]
    Everything works as it should. To see changes within the app after
    enabling/disabling this switch, app must be restarted everytime switch
    is toggled.
    
    Change-Id: I696f6abe8007e1d84af5a8ccc675dce427fb8d2d
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4330,6 +4330,12 @@ public final class Settings {
         public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
 
         /**
+         * Dyanamic Navbar Switch
+         * @hide
+         */
+        public static final String NAV_BAR_DYNAMIC = "nav_bar_dynamic";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
