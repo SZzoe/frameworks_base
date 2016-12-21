ef0c3a3ca272fe622dc4521c4c5b4b3d7eff2b73
ezio84 <brabus84@gmail.com>
2016-12-21 12:49:34 +0100

    Power connect/disconnect notif: fix sound playing when set to silent
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -67,6 +67,8 @@ public class PowerUI extends SystemUI {
     // For filtering ACTION_POWER_DISCONNECTED on boot
     boolean mIgnoreFirstPowerEvent = true;
 
+    private static final String POWER_NOTIFICATIONS_SILENT_URI = "silent";
+
     public void start() {
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mScreenOffTime = mPowerManager.isScreenOn() ? -1 : SystemClock.elapsedRealtime();
@@ -242,7 +244,7 @@ public class PowerUI extends SystemUI {
         final String soundPath =
                 CMSettings.Global.getString(cr, CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE);
 
-        if (soundPath != null) {
+        if (soundPath != null && !soundPath.equals(POWER_NOTIFICATIONS_SILENT_URI) ) {
             Ringtone powerRingtone = RingtoneManager.getRingtone(mContext, Uri.parse(soundPath));
             if (powerRingtone != null) {
                 powerRingtone.play();
