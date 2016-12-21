c677e522f1b0d23da45ca53c8afb444e07ce00c5
Janson Kang <temasek71@gmail.com>
2016-12-21 23:20:19 +0100

    FWB: Clock font style (SQUASHED)
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4409,6 +4409,12 @@ public final class Settings {
         public static final String APP_SIDEBAR_HIDE_TIMEOUT = "app_sidebar_hide_timeout";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
