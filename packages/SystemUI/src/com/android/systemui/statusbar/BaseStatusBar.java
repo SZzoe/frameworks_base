948513661f6524e9cbd7cf4cdc05fe7040904fc7
Pafcholini <nadyaivanova14@gmail.com>
2017-01-25 03:40:05 +0100

    Statusbar: add missing import
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -36,6 +36,7 @@ import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
